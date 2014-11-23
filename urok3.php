b7be7978a692b8d5343c29ccbd138b7ec650354d
Alex Martynov <alex-kwon@mail.ru>
2014-11-23 23:31:02 +0200

    Urok3

--- urok3.php
+++ urok3.php
@@ -32,43 +32,89 @@ $date = array('data1' => rand(0,time()),
 echo "<br>";
     
 //Наименьший день
-    echo '<h4>'; echo 'Наименьший день в сгенерированном массиве: '; echo '</4>';     
-    echo min(date("d", $date['data1']), date("d", $date['data2']),date("d", $date['data3']),date("d", $date['data4']),date("d", $date['data5'])) . '<br>';
+    echo '<h4>'; echo 'Наименьший день в сгенерированном массиве: '; echo '</h4>'; 
+    $date_low_day = array('data1' => rand(0,time()),
+                          'data2' => rand(0,time()),
+                          'data3' => rand(0,time()),
+                          'data4' => rand(0,time()),
+                          'data5' => rand(0,time())
+              );
+    echo 'Наименьший день - ' . min(date("d", $date_low_day['data1']), 
+                                    date("d", $date_low_day['data2']),
+                                    date("d", $date_low_day['data3']),
+                                    date("d", $date_low_day['data4']),
+                                    date("d", $date_low_day['data5'])) . '<br>';
+    echo "<br>";
+    echo 'Дата 1: ' . 'Unix time: ' . $date_low_day['data1'] . ' GMT: ' . date("d/m/Y", $date_low_day['data1']);
+         echo "<br>";
+    echo 'Дата 2: ' . 'Unix time: ' . $date_low_day['data2'] . ' GMT: ' . date("d/m/Y", $date_low_day['data2']);
+         echo "<br>";
+    echo 'Дата 3: ' . 'Unix time: ' . $date_low_day['data3'] . ' GMT: ' . date("d/m/Y", $date_low_day['data3']);
+         echo "<br>";
+    echo 'Дата 4: ' . 'Unix time: ' . $date_low_day['data4'] . ' GMT: ' . date("d/m/Y", $date_low_day['data4']);
+         echo "<br>";
+    echo 'Дата 5: ' . 'Unix time: ' . $date_low_day['data5'] . ' GMT: ' . date("d/m/Y", $date_low_day['data5']);
 
 //Наибольший месяц
-    echo '<h4>'; echo 'Наибольший месяц в сгенерированном массиве: '; echo '</4>';     
-    echo max(date("m", $date['data1']), date("m", $date['data2']),date("m", $date['data3']),date("m", $date['data4']),date("m", $date['data5'])) . '<br>';
-
+    echo '<h4>'; echo 'Наибольший месяц в сгенерированном массиве: '; echo '</h4>';
+    $date_high_mon = array('data1' => rand(0,time()),
+                           'data2' => rand(0,time()),
+                           'data3' => rand(0,time()),
+                           'data4' => rand(0,time()),
+                           'data5' => rand(0,time())
+              );
+    echo 'Наибольший месяц - ' . max(date("m", $date_high_mon['data1']), 
+                                     date("m", $date_high_mon['data2']),
+                                     date("m", $date_high_mon['data3']),
+                                     date("m", $date_high_mon['data4']),
+                                     date("m", $date_high_mon['data5'])) . '<br>';
+    echo "<br>";
+    echo 'Дата 1: ' . 'Unix time: ' . $date_high_mon['data1'] . ' GMT: ' . date("d/m/Y", $date_high_mon['data1']);
+         echo "<br>";
+    echo 'Дата 2: ' . 'Unix time: ' . $date_high_mon['data2'] . ' GMT: ' . date("d/m/Y", $date_high_mon['data2']);
+         echo "<br>";
+    echo 'Дата 3: ' . 'Unix time: ' . $date_high_mon['data3'] . ' GMT: ' . date("d/m/Y", $date_high_mon['data3']);
+         echo "<br>";
+    echo 'Дата 4: ' . 'Unix time: ' . $date_high_mon['data4'] . ' GMT: ' . date("d/m/Y", $date_high_mon['data4']);
+         echo "<br>";
+    echo 'Дата 5: ' . 'Unix time: ' . $date_high_mon['data5'] . ' GMT: ' . date("d/m/Y", $date_high_mon['data5']);
+    
 //Сортировка массива по возрастанию дат
-    echo '<h4>'; echo 'Сортировка массива по возрастанию дат: '; echo '</4>';  
-    echo array_multisort($date, SORT_DESC);
-        echo "<br>";
-             echo '<pre>';
-                 print_r($date);
-             echo '</pre>';
+    echo '<h4>'; echo 'Сортировка массива по возрастанию дат: </b><br></h4>';
+    echo 'Массив до сортировки: <br>';
+      echo '<pre>';
+        print_r($date);
+      echo '</pre>';
+    echo 'Массив после сортировки: <br><br>';
+    arsort($date);
+    foreach($date as $key => $value) {
+        echo $key  . ' Unix time: ' . "$value" . ' GMT: ' . date("d/m/Y", $value) . '</b><br>';
+      }
 
 //Извлечение последнего элемента массива в новую переменную $selected
-    echo '<h4>'; echo 'Извлечение последнего элемента массива в новую переменную $selected: '; echo '</4>';  
+    echo '<h4>'; echo 'Извлечение последнего элемента массива в новую переменную $selected: '; echo '</h4>';  
+    echo 'Массив до сортировки: <br><br>';
+        foreach($date as $key => $value) {
+            echo $key  . ' Unix time: ' . "$value" . ' GMT: ' . date("d/m/Y", $value) . '</b><br>';
+        }
+    echo "<br>";
+    echo 'Массив после сортировки: <br><br>';
     $selected = array_pop($date);
-             echo "<br>";
              echo '<pre>';
                  print_r($date);
-             echo '</pre>';
-    echo "<br>";         
-    echo '$selected = ' . $selected;
+             echo '</pre>';         
+    echo '$selected = ' . date("d/m/Y", $selected);
 
 //Преобразование переменной $selected в формат "дд.мм.ГГ ЧЧ:ММ:СС"
-echo '<h4>'; echo 'Преобразование переменной $selected в формат "дд.мм. ГГ ЧЧ:ММ:СС": '; echo '</4>';  
+echo '<h4>'; echo 'Преобразование переменной $selected в формат "дд.мм. ГГ ЧЧ:ММ:СС": '; echo '</h4>';  
 echo date("d.m.Y H:i:s", $selected);
 
 //Установка часовой пояса для Нью-Йорка:
 echo '<h4>'; echo 'Установка часовой пояса для Нью-Йорка: '; echo '</4>'  . "<br>" . "<br>"; 
 echo 'Текущий часовой пояс - ' . date_default_timezone_get() . "<br>";
 echo 'Текущее время - ' . date("d/m/Y H:i:s") . "<br>" . "<br>";
+
 //Изменил на Американский ЧП
 date_default_timezone_set('America/New_york');
 echo 'Новый часовой пояс - ' . date_default_timezone_get() . "<br>";
 echo 'Время в Нью-Йорке: ' .' '. date("d/m/Y H:i:s");
-
-
-?>
